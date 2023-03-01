0fbcfd15842058d88206a964bf66092d4316732e
Alexey Zayats <alekseyzayats.h8@gmail.com>
2023-03-01 12:58:48 +0300

    Digits

---
+++ Lesson_1(Digits)/main.py
@@ -0,0 +1,18 @@
+# однострочный комментарий - one line comment
+"""
+    многострочный комментарий
+            -
+    multiline comment
+"""
+
+print(2 + 3)  # сложение - addition
+print(3 - 2)  # вычитание - substraction
+print(2 * 3)  # умножение - multiplication
+print(3/2)  # деление - division
+print(9//2)  # деление без остатка - division without remainder
+print(9 % 4)  # остаток - remainder
+print(2**3)  # возведение в степень - raised to the power
+
+a = 2**8  # перменная - variable
+b = a  # присвоение значени - value assigment
+print(b)
